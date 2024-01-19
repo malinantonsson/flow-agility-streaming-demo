782d2786d6690a5845f34aaf36758666e0688fe5
Malin Antonsson <mantonsson90@gmail.com>
2024-01-19 21:37:25 +0100

    feat: upgrade

---
+++ js/app.js
@@ -0,0 +1,269 @@
+//const WEBSOCKET_URL = "ws:facom-stage.fly.dev/ws/streaming/-HUwjLmF"
+
+// Get the websocket url from Flow agility and paste it here
+// it should look something like this: "ws:facom-stage.fly.dev/ws/streaming/7nt5UYKh"
+// you may have to add the "ws:" bit
+const WEBSOCKET_URL = "wss:flowagility.com/ws/streaming/HL8cGNWY";
+
+// https://facom-stage.fly.dev/organizer/events
+
+const updateRunData = (name, playset, prefix = "streaming-run") => {
+  document.querySelector(`#${prefix}-manga`).innerHTML = name;
+  document.querySelector(`#${prefix}-start_order`).innerHTML =
+    playset.start_order;
+  document.querySelector(`#${prefix}-dog_name`).innerHTML =
+    `${playset.dog_family_name}, `;
+  document.querySelector(`#${prefix}-handler_name`).innerHTML = playset.handler;
+  document.querySelector(`#${prefix}-club_name`).innerHTML = playset.club;
+
+  document.querySelector(`#${prefix}-dog_faults`).innerHTML = playset.faults;
+
+  document.querySelector(`#${prefix}-dog_refusals`).innerHTML =
+    playset.refusals;
+
+  document.querySelector(`#${prefix}-dog_time`).innerHTML = playset.time
+    ? `${playset.time}`
+    : "";
+
+  if (playset.disqualification === "elim") {
+    document.querySelector(`#${prefix}-pen`).style.opacity = "0";
+    document.querySelector(`#${prefix}-dog_elim`).style.opacity = "1";
+  } else {
+    document.querySelector(`#${prefix}-dog_elim`).style.opacity = "0";
+    document.querySelector(`#${prefix}-pen`).style.opacity = "1";
+  }
+};
+
+const toggleRunData = (isShow) => {
+  document.querySelector("#streaming-run").style.display = isShow
+    ? "block"
+    : "none";
+};
+
+const toggleRunResult = (isShow) => {
+  document.querySelector("#streaming-run-result").style.display = isShow
+    ? "block"
+    : "none";
+};
+
+const hideEverything = () => {
+  document.getElementById("streaming").style.display = "none";
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("results").style.display = "none";
+};
+
+const showStreaming = () => {
+  document.getElementById("streaming").style.display = "block";
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("results").style.display = "none";
+};
+
+const showResults = (results = {}) => {
+  if (results.results && results.results.length > 0) {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("streaming").style.display = "none";
+    document.getElementById("results").style.display = "block";
+
+    document.getElementById("results-header").innerHTML = results.title;
+
+    let resultHtml = ``;
+    results.results.forEach((r, index) => {
+      resultHtml =
+        resultHtml +
+        `<tr id="result-template">
+              <td>
+              <span data-id="results-item-order">${index + 1}.</span>
+              </td>
+              <td>
+              <span data-id="results-item-dog" class="results-item-dog">${
+                r.dog_family_name
+              }</span>
+              </td>
+              <td>
+                
+                  
+                  
+                  <span data-id="results-item-handler" class="results-item-handler">${
+                    r.handler
+                  }</span>
+              </td>
+              <td>${r.time}</td>
+              <td>${r.total_penalization}</td>
+            </tr>`;
+    });
+    document.getElementById("results-body").innerHTML = resultHtml;
+  }
+};
+
+const showIntro = () => {
+  document.getElementById("intro").style.display = "block";
+  document.getElementById("streaming").style.display = "none";
+  document.getElementById("results").style.display = "none";
+};
+
+const showRunError = (msg) => {
+  document.querySelector(`#streaming-run-club_name`).innerHTML = msg;
+};
+
+(function () {
+  let results = {};
+
+  const connectToSocket = (websocketUrl) => {
+    // STATUSES
+    const err = document.getElementById("error");
+    const closed = document.getElementById("closed");
+    const status = document.getElementById("status");
+
+    console.log("TRY TO CONNECT: ", websocketUrl);
+    const showConnectionError = () => {
+      isSocketOpen = false;
+
+      err.style.display = "block";
+      status.innerHTML = "not connected";
+    };
+
+    // Create WebSocket connection.
+    const socket = new WebSocket(websocketUrl);
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+      console.log("CONNECTED");
+      isSocketOpen = true;
+      // always request the first data on load
+      socket.send("streaming_data");
+
+      status.innerHTML = "connected";
+      err.style.display = "none";
+      closed.style.display = "none";
+
+      // send ping every 30 seconds to keep connection open
+      setInterval(() => {
+        console.log("SEND PING");
+        socket.send("ping");
+      }, 30000);
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.log("CLOSED");
+      isSocketOpen = false;
+      status.innerHTML = "not connected";
+    });
+
+    socket.addEventListener("error", (event) => {
+      console.log("ERROR");
+      showConnectionError();
+    });
+
+    // Listen for messages
+    socket.addEventListener("message", (event) => {
+      console.log("Message from server ", event);
+      if (event.data === "pong") return;
+      console.log("data: ", JSON.parse(event.data));
+      const data = JSON.parse(event.data);
+
+      const { run, run_ready, error } = data;
+
+      if (error) {
+        showRunError(error);
+      }
+
+      if (!run && run_ready) {
+        // if there is no `run` use `run_ready` - dog has not started yet
+        // When SO of the first run for the date/ring is set
+        console.log("When SO of the first run for the date/ring is set");
+
+        updateRunData(run_ready.name, run_ready.playset);
+      }
+
+      // dog is running
+      if (run) {
+        // 10. When 2nd run-pair last playset is submitted
+        const isCombinedReady = run.results_combined.length > 0;
+        results = {
+          results: isCombinedReady ? run.results_combined : run.results_best,
+          title: `${run.name} ${
+            isCombinedReady ? "Combined results" : "Best results"
+          }`,
+        };
+
+        // 3. When first playset is getting the first Fault
+        // 4. When first playset is getting the Refusal
+        // 4. When first playset is disqualified by the Judge
+        // 8. When 2nd run-pair 1st playset got first Fault
+        if (run.playset.status_string === "running") {
+          updateRunData(run.name, run.playset);
+        }
+
+        // run is complete
+        if (run.playset.status_string === "calculated") {
+          // 5. When first playset is submitted with 2F and 1R and time of 23.56
+          // 6. When second playset of the 2-playsets run (1st run-pair - without combined results) is submitted
+          // 7. When the 2nd run-pair SO is set (run-ready information is showing the run which is ready to start)
+          // 9. When 2nd run-pair 1st playset is submitted
+
+          // setting run results
+          updateRunData(run.name, run.playset, "streaming-run-result");
+          // showing run results
+          toggleRunResult(true);
+          toggleRunData(false);
+
+          // show results for 5s, then get the data for the next dog
+          // update run detail with next run in background
+          if (run_ready) {
+            updateRunData(run_ready.name, run_ready.playset);
+          }
+
+          setTimeout(() => {
+            toggleRunResult(false);
+            toggleRunData(true);
+          }, 5000);
+
+          // the manga has finished
+          // 7. When the 2nd run-pair SO is set (run-ready information is showing the run which is ready to start)
+          if (run?.name !== run_ready?.name || run?.type !== run_ready?.type) {
+            // do nothing?
+            // user can manually go to result screen
+          }
+        }
+      }
+    });
+  };
+
+  let params = new URLSearchParams(document.location.search);
+  let page = params.get("page");
+  let url = params.get("url");
+  const websocketUrl = url || WEBSOCKET_URL;
+
+  if (page) {
+    if (page === "stream") {
+      showStreaming();
+    }
+  }
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "s" || evt.key === "S") {
+      showStreaming();
+    }
+
+    if (evt.key === "r" || evt.key === "R") {
+      showResults(results);
+    }
+
+    if (evt.key === "Backspace" || evt.key === "ArrowLeft") {
+      showIntro();
+    }
+
+    if (evt.key === "n" || evt.key === "N") {
+      socket.send("streaming_data");
+    }
+
+    if (evt.key === "h" || evt.key === "H") {
+      hideEverything();
+    }
+  });
+
+  try {
+    connectToSocket(websocketUrl);
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
+})();
---
+++ js/plugins.js
@@ -0,0 +1,406 @@
+// Avoid `console` errors in browsers that lack a console.
+(function () {
+  var method;
+  var noop = function () {};
+  var methods = [
+    "assert",
+    "clear",
+    "count",
+    "debug",
+    "dir",
+    "dirxml",
+    "error",
+    "exception",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "markTimeline",
+    "profile",
+    "profileEnd",
+    "table",
+    "time",
+    "timeEnd",
+    "timeline",
+    "timelineEnd",
+    "timeStamp",
+    "trace",
+    "warn",
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+})();
+
+// Place any jQuery/helper plugins in here.
+
+/**
+ * Replacement for toFixed, but trunk instead of round
+ * @param {float} value Value to be parsed,
+ * @param {int} numdecs Number of decimal numbers to be shown
+ * no, cannot use sprintf library, as internally uses toFixed() rounding
+ */
+function toFixedT(value, numdecs) {
+  // first approach. may fail with some numbers due to internal handling of floating point
+  // numbers in javascript: ie: toFixedT(4.27 , 2 ) return 4.26 due to js internal handling
+  if (typeof value === "undefined") return "";
+  if (value == null) return "";
+  if (value === "") return "";
+  if (isNaN(value)) return value;
+  if (value < 0) value = 0; // PENDING should not happen. need to study
+  /*
+	return Number ( value - 5/Math.pow(10,numdecs+1)).toFixed(numdecs);
+	*/
+  var str = "";
+  switch (parseInt(numdecs)) {
+    case 0:
+      return parseInt(value);
+    case 1:
+      str = value.toString().match(/^\d+(?:\.\d{0,1})?/);
+      break;
+    case 2:
+      str = value.toString().match(/^\d+(?:\.\d{0,2})?/);
+      break;
+    case 3:
+      str = value.toString().match(/^\d+(?:\.\d{0,3})?/);
+      break;
+    case 4:
+      str = value.toString().match(/^\d+(?:\.\d{0,4})?/);
+      break;
+    default:
+      return value.toFixed(numdecs); // use default javascript routine
+  }
+  // now complete number of decimals reqired
+  if (str == "0") str = "0.00001"; // very, very, very stupid javascript
+  if (str.toString().indexOf(".") < 0) str = str + ".00001";
+  else str = str + "00001";
+  var res = str.split(".");
+  return res[0] + "." + res[1].substr(0, numdecs);
+}
+
+/* ------------------------------------------------------------------------
+ * 
+ * Reference: https://github.com/20tab/jquery-chron
+ * 
+------------------------------------------------------------------------- */
+
+(function ($) {
+  var running = false;
+  var paused = false;
+  var startTime = 0;
+  var localTime = 0; // to avoid time offset problems
+  var stopTime = 0;
+  var pauseTime = 0;
+
+  var config = {
+    // initial values
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    mseconds: 0,
+
+    // HTML element ID's to work with (display objects)
+    days_sel: "#days",
+    hours_sel: "#hours",
+    minutes_sel: "#minutes",
+    seconds_sel: "#seconds",
+    mseconds_sel: "#mseconds",
+
+    // HTML element ID's to work with (button objects)
+    start: "#start",
+    stop: "#stop",
+    reset: "#reset",
+    pause: "#pause",
+    resume: "#resume",
+
+    // methods to be called before action taken. on return false skip action
+    onBeforeStart: function () {
+      return true;
+    },
+    onBeforeStop: function () {
+      return true;
+    },
+    onBeforeReset: function () {
+      return true;
+    },
+    onBeforeResync: function () {
+      return true;
+    },
+    onBeforePause: function () {
+      return true;
+    },
+    onBeforeResume: function () {
+      return true;
+    },
+    onUpdate: function (tstamp, running, paused) {
+      return true;
+    }, // action to do on display new timestamp
+
+    triggerEvents: false, // on true triggerEvents
+    target: "*", //selectors for the events target
+    auto: true, //true if plugin generate html chronometer
+    interval: 100, // polling interval (msecs) default: 0.1 second
+    showMode: 0, // 0: use hh:mm:ss.xxx format else use decimal seconds format with provided precision
+  };
+
+  var methods = {
+    init: function (options) {
+      $.extend(config, options);
+    },
+    start: function (timestamp) {
+      var check = config.onBeforeStart();
+      if (check != false) {
+        $(config.start).attr("disabled", true);
+        $(config.stop).attr("disabled", false);
+        $(config.resume).attr("disabled", true);
+        $(config.pause).attr("disabled", false);
+        localTime = Date.now();
+        if (typeof timestamp === "undefined") startTime = localTime;
+        else startTime = timestamp;
+        stopTime = startTime;
+        pauseTime = startTime;
+        running = true;
+        paused = false;
+        run_chrono();
+      }
+      if (config.triggerEvents) $(config.target).trigger("chronostart");
+    },
+    stop: function (timestamp) {
+      var check = config.onBeforeStop();
+      if (check != false) {
+        $(config.start).attr("disabled", false);
+        $(config.stop).attr("disabled", true);
+        $(config.resume).attr("disabled", true);
+        $(config.pause).attr("disabled", true);
+        if (typeof timestamp === "undefined") stopTime = Date.now();
+        else stopTime = timestamp;
+        running = false;
+        paused = false;
+      }
+      if (config.triggerEvents) $(config.target).trigger("chronostop");
+    },
+    pause: function (timestamp) {
+      var check = config.onBeforePause();
+      if (check != false) {
+        $(config.start).attr("disabled", false);
+        $(config.stop).attr("disabled", true);
+        $(config.resume).attr("disabled", false);
+        $(config.pause).attr("disabled", true);
+        if (typeof timestamp === "undefined") pauseTime = Date.now();
+        else pauseTime = timestamp;
+        running = false;
+        paused = true;
+      }
+      if (config.triggerEvents) $(config.target).trigger("chronopause");
+    },
+    resume: function () {
+      var check = config.onBeforeResume();
+      if (check != false) {
+        $(config.start).attr("disabled", true);
+        $(config.stop).attr("disabled", false);
+        $(config.resume).attr("disabled", true);
+        $(config.pause).attr("disabled", false);
+        running = true;
+        paused = false;
+        run_chrono();
+      }
+      if (config.triggerEvents) $(config.target).trigger("chronoresume");
+    },
+    resync: function (timestop, timestart) {
+      var check = config.onBeforeResync();
+      if (check != false) {
+        var elapsed = timestop - startTime; // time of manual chrono
+        startTime = timestart - elapsed; // adjust start time on auto chrono
+      }
+      if (config.triggerEvents) $(config.target).trigger("chronoresync");
+    },
+    reset: function () {
+      var check = config.onBeforeReset();
+      if (check != false) {
+        // set elapsed time to 0. dont stop/pause if running
+        startTime = 0;
+        stopTime = 0;
+        localTime = 0;
+      }
+      if (config.triggerEvents) $(config.target).trigger("chronoreset");
+    },
+    started: function () {
+      return running || paused;
+    },
+    getValue: function () {
+      if (paused) return pauseTime - startTime; // paused: show intermediate time
+      if (!running) return stopTime - startTime; // stopped: show last stored time mark
+      return Date.now() - localTime; // running. may be not accurate
+    },
+  };
+
+  function run_chrono() {
+    var now = Date.now();
+    var elapsed = 0;
+    if (startTime == 0) startTime = now;
+    if (localTime == 0) localTime = now;
+    if (stopTime == 0) stopTime = now;
+    if (pauseTime == 0) pauseTime = now;
+    if (paused) {
+      elapsed = pauseTime - startTime; // use localTime to evaluate time lapse
+      config.mseconds = elapsed % 1000;
+      config.seconds = Math.floor(elapsed / 1000);
+      config.minutes = Math.floor(config.seconds / 60);
+      config.seconds = config.seconds % 60;
+      config.hours = Math.floor(config.minutes / 60);
+      config.minutes = config.minutes % 60;
+      config.days = Math.floor(config.hours / 24);
+      config.hours = config.hours % 24;
+    } else if (running) {
+      elapsed = now - localTime; // use localTime to evaluate time lapse
+      config.mseconds = elapsed % 1000;
+      config.seconds = Math.floor(elapsed / 1000);
+      config.minutes = Math.floor(config.seconds / 60);
+      config.seconds = config.seconds % 60;
+      config.hours = Math.floor(config.minutes / 60);
+      config.minutes = config.minutes % 60;
+      config.days = Math.floor(config.hours / 24);
+      config.hours = config.hours % 24;
+      setTimeout(run_chrono, config.interval);
+    } else {
+      // chrono stopped; show data at least once
+      elapsed = stopTime - startTime; // use real startTime instead of localTime
+      config.mseconds = elapsed % 1000;
+      config.seconds = Math.floor(elapsed / 1000);
+      config.minutes = Math.floor(config.seconds / 60);
+      config.seconds = config.seconds % 60;
+      config.hours = Math.floor(config.minutes / 60);
+      config.minutes = config.minutes % 60;
+      config.days = Math.floor(config.hours / 24);
+      config.hours = config.hours % 24;
+    }
+    view_chrono(elapsed);
+  }
+
+  function view_chrono(elapsed) {
+    if (elapsed < 0) elapsed = 0; // to avoid bogus data in screen
+    if (!config.onUpdate(elapsed, running, paused)) return;
+    var digits = config.showMode;
+    var extra = "";
+    if (running && config.showMode > 0) {
+      digits = config.showMode - 1;
+      extra = "&nbsp;"; // add an space
+    }
+
+    if (config.showMode == 0) {
+      $(config.days_sel).html(view_format(config.days));
+      $(config.days_sel).data("days", config.days);
+      $(config.hours_sel).html(view_format(config.hours));
+      $(config.hours_sel).data("hours", config.hours);
+      $(config.minutes_sel).html(view_format(config.minutes));
+      $(config.minutes_sel).data("minutes", config.minutes);
+      $(config.seconds_sel).html(view_format(config.seconds));
+      $(config.seconds_sel).data("seconds", config.seconds);
+      $(config.mseconds_sel).html(view_format(config.mseconds));
+      $(config.mseconds_sel).data("mseconds", config.mseconds);
+    } else {
+      // ignore all fields but "seconds" and show in floating point with provided precision
+      $(config.days_sel).html("");
+      $(config.days_sel).data("days", 0);
+      $(config.hours_sel).html("");
+      $(config.hours_sel).data("hours", 0);
+      $(config.minutes_sel).html("");
+      $(config.minutes_sel).data("minutes", 0);
+      $(config.seconds_sel).html(
+        "" + toFixedT(parseFloat(elapsed / 1000.0), digits) + extra
+      );
+      $(config.seconds_sel).data("seconds", parseFloat(elapsed / 1000.0));
+      $(config.mseconds_sel).html("");
+      $(config.mseconds_sel).data("mseconds", 0);
+    }
+  }
+
+  function view_format(value) {
+    return value < 10 ? "0" + value : value;
+  }
+
+  function format_selector(value) {
+    return value.replace("#", "").replace(".", "");
+  }
+
+  $.fn.Chrono = function (args) {
+    if (methods[args]) {
+      // vemos si lo hemos invocado con un metodo como parametro
+      return methods[args].apply(
+        this,
+        Array.prototype.slice.call(arguments, 1)
+      );
+    } else if (typeof args === "object" || !args) {
+      // init con argumentos de configuracion
+      methods.init.apply(this, arguments);
+      return this.each(function () {
+        // if automatic mode is set, consider this object as an HTML element and expand proper html code
+        if (config.auto) {
+          $(this).html(
+            '<span id="' +
+              format_selector(config.days_sel) +
+              '" data-days = "' +
+              config.days +
+              '">' +
+              view_format(config.days) +
+              '</span>:<span id="' +
+              format_selector(config.hours_sel) +
+              '" data-hours = "' +
+              config.hours +
+              '">' +
+              view_format(config.hours) +
+              '</span>:<span id="' +
+              format_selector(config.minutes_sel) +
+              '" data-minutes = "' +
+              config.minutes +
+              '">' +
+              view_format(config.minutes) +
+              '</span>:<span id="' +
+              format_selector(config.seconds_sel) +
+              '" data-seconds = "' +
+              config.seconds +
+              '">' +
+              view_format(config.seconds) +
+              "</span>"
+          );
+        }
+        if (config.showMode < 0) config.showMode = 0;
+        if (
+          config.days != 0 ||
+          config.hours != 0 ||
+          config.minutes != 0 ||
+          config.seconds != 0 ||
+          config.mseconds != 0
+        ) {
+          // if initial data are not null assume clock assume chrono in "started" state
+          $(config.start).attr("disabled", true);
+          $(config.pause).attr("disabled", true);
+        } else {
+          // else assume clock is created in "stopped" state
+          $(config.stop).attr("disabled", true);
+          $(config.pause).attr("disabled", true);
+          $(config.resume).attr("disabled", true);
+        }
+        // set up events handlers
+        $(config.start).on("click", methods.start);
+        $(config.stop).on("click", methods.stop);
+        $(config.reset).on("click", methods.reset);
+        $(config.pause).on("click", methods.pause);
+        $(config.resume).on("click", methods.resume);
+      });
+    } else {
+      // error
+      $.error("Method " + args + " does not exist on jQuery.Chrono");
+    }
+  };
+})(jQuery);
